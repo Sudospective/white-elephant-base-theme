92c6c28920592e57b1efb5161ce1021859b52736
Sudospective <avery@sudospective.net>
2024-09-09 11:17:40 -0700

    Initial commit

---
+++ BGAnimations/WEIntro overlay/default.lua
@@ -0,0 +1,31 @@
+local af = Def.ActorFrame {
+  InitCommand = function(self)
+    self
+      :diffuse(0.75, 0.75, 0.75, 1)
+      :FullScreen()
+  end,
+}
+
+af[#af + 1] = Def.Text {
+  Font = "GreyQo",
+  Text = "Sea of Clouds\nPresents",
+  Size = 48,
+  StrokeSize = 2,
+  InitCommand = function(self)
+    self:MainActor()
+      :diffuse(0, 0, 0, 1)
+    self:StrokeActor()
+      :diffuse(1, 1, 1, 1)
+    self
+      :Regen()
+      :diffusealpha(0)
+      :sleep(1)
+      :linear(1)
+      :diffusealpha(1)
+      :sleep(3)
+      :linear(1)
+      :diffusealpha(0)
+  end,
+}
+
+return af
