cfbbe163f0f9d0d714e6dd0b7ff09847dbfa6aba
Sudospective <avery@sudospective.net>
2024-09-09 11:30:27 -0700

    oh boy here we go

--- BGAnimations/WEIntro overlay/default.lua
+++ BGAnimations/WEIntro overlay/default.lua
@@ -1,4 +1,6 @@
-local af = Def.ActorFrame {
+local af = Def.ActorFrame {}
+
+af[#af + 1] = Def.Quad {
   InitCommand = function(self)
     self
       :diffuse(0.75, 0.75, 0.75, 1)
@@ -7,11 +9,12 @@ local af = Def.ActorFrame {
 }
 
 af[#af + 1] = Def.Text {
-  Font = "GreyQo",
+  Font = THEME:GetPathF("", "GreyQo.ttf"),
   Text = "Sea of Clouds\nPresents",
-  Size = 48,
+  Size = 64,
   StrokeSize = 2,
   InitCommand = function(self)
+    self:Center()
     self:MainActor()
       :diffuse(0, 0, 0, 1)
     self:StrokeActor()
